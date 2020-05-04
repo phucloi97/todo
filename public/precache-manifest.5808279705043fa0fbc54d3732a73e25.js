3ed0bab1655b689472c7afbaf8c57199388ee140
phucloi97 <phucloi97@hotmail.com>
2020-05-04 20:46:11 +0700

    1.0001

---
+++ public/precache-manifest.5808279705043fa0fbc54d3732a73e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2159cde8036c20d9659aca966f92b671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4b1be3f609fe6adda89",
+    "url": "/static/css/2.b98d5fdb.chunk.css"
+  },
+  {
+    "revision": "eb43d38a643a34d3f6e5",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c4b1be3f609fe6adda89",
+    "url": "/static/js/2.ec5a690f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ec5a690f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb43d38a643a34d3f6e5",
+    "url": "/static/js/main.12045765.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
